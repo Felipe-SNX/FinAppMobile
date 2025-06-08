d4ad42c3071f189c7ce4ad029babe28d3876489a
unknown <felipesantossnx@gmail.com>
2025-06-08 19:24:07 -0300

    Adição de títulos e formatação de moeda

--- app/src/main/java/TransacaoAdapter.kt
+++
@@ -1,5 +0,0 @@
-package com.example.finapp
-
-// Supondo que você tenha uma classe de dados como esta
-// data class Transacao(val tipo: String, val descricao: String, val valor: Double)
-
--- app/src/main/java/com/example/finapp/controller/CadastroActivity.kt
+++ app/src/main/java/com/example/finapp/controller/CadastroActivity.kt
@@ -1,6 +1,7 @@
 package com.example.finapp.controller
 
 import android.os.Bundle
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioGroup
@@ -10,6 +11,8 @@ import com.example.finapp.R
 import com.example.finapp.com.example.finapp.TipoOperacao
 import com.example.finapp.data.dao.OperacaoDao
 import com.example.finapp.model.OperacaoModel
+import android.text.Editable
+import java.text.DecimalFormat
 
 class CadastroActivity : AppCompatActivity() {
     private lateinit var operacaoDao: OperacaoDao
@@ -59,5 +62,42 @@ class CadastroActivity : AppCompatActivity() {
             Toast.makeText(this, "Operação cadastrada!", Toast.LENGTH_SHORT).show()
             finish()
         }
+
+        //Formata o valor para duas casas decimais
+        etValor.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(editable: Editable?) {
+                etValor.removeTextChangedListener(this);
+
+                val stringOriginal = editable.toString();
+                val separadorDecimal = DecimalFormat().decimalFormatSymbols.decimalSeparator;
+                var textoLimpo = stringOriginal.replace(".", separadorDecimal.toString());
+
+                val temVirgula = textoLimpo.indexOf(separadorDecimal);
+
+                if (temVirgula >= 0) {
+                    val integerPart = textoLimpo.substring(0, temVirgula);
+                    var decimalPart = textoLimpo.substring(temVirgula + 1);
+
+                    if (decimalPart.length > 2) {
+                        decimalPart = decimalPart.substring(0, 2);
+                    }
+
+                    val newText = "$integerPart$separadorDecimal$decimalPart";
+                    etValor.setText(newText);
+                    etValor.setSelection(newText.length);
+                } else {
+                    etValor.setText(textoLimpo);
+                    etValor.setSelection(textoLimpo.length);
+                }
+
+                etValor.addTextChangedListener(this)
+            }
+        });
     }
 }
