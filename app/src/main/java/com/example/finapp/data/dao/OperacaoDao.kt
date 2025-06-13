514525904699c5fc9257c6cbf8e9fc95cca8dd43
unknown <felipesantossnx@gmail.com>
2025-06-13 10:33:14 -0300

    Refatoração em OperacaoDao

--- app/src/main/java/com/example/finapp/data/dao/OperacaoDao.kt
+++ app/src/main/java/com/example/finapp/data/dao/OperacaoDao.kt
@@ -6,41 +6,36 @@ import android.database.Cursor
 import com.example.finapp.model.enum.TipoOperacao
 import com.example.finapp.data.db.DBHelper
 import com.example.finapp.model.OperacaoModel
+import kotlin.collections.mutableListOf
 
 class OperacaoDao (private val context: Context){
     private val dbHelper = DBHelper(context);
 
     fun addOperacao(operacao: OperacaoModel): Long {
-        val db = dbHelper.writableDatabase
+        val db = dbHelper.writableDatabase;
         val values = ContentValues().apply {
             put("descricao", operacao.descricao)
             put("valor", operacao.valor)
             put("tipoOperacao", operacao.tipoOperacao.name)
-        }
-        val id = db.insert(DBHelper.TABLE_NAME, null, values)
-        db.close()
-        return id
+        };
+        val id = db.insert(DBHelper.TABLE_NAME, null, values);
+        db.close();
+        return id;
     }
 
     fun getAllOperacoes(): List<OperacaoModel>{
-        val db = dbHelper.readableDatabase
-        val cursor: Cursor = db.query(DBHelper.TABLE_NAME, null, null, null, null, null, null)
-        val operacaoList = mutableListOf<OperacaoModel>()
-        while(cursor.moveToNext()){
-            val id = cursor.getInt(cursor.getColumnIndexOrThrow("id"))
-            val descricao = cursor.getString(cursor.getColumnIndexOrThrow("descricao"))
-            val valor = cursor.getDouble(cursor.getColumnIndexOrThrow("valor"))
-            val stringTipoOperacao = cursor.getString(cursor.getColumnIndexOrThrow("tipoOperacao"))
-            val tipoOperacao = TipoOperacao.valueOf(stringTipoOperacao);
-            operacaoList.add(OperacaoModel(id,descricao,valor,tipoOperacao))
-        }
-        cursor.close()
-        db.close()
-        return operacaoList
+        val db = dbHelper.readableDatabase;
+        val cursor: Cursor = db.query(DBHelper.TABLE_NAME, null, null, null, null, null, null);
+        val operacaoList = mutableListOf<OperacaoModel>();
+        adicionaLista(cursor, operacaoList);
+
+        cursor.close();
+        db.close();
+        return operacaoList;
     }
 
     fun getAllByTipo(tipoBusca: TipoOperacao): List<OperacaoModel>{
-        val db = dbHelper.readableDatabase
+        val db = dbHelper.readableDatabase;
         val cursor: Cursor = db.query(
             DBHelper.TABLE_NAME,
             null,
@@ -50,18 +45,22 @@ class OperacaoDao (private val context: Context){
             null,
             null
         );
-        val operacaoList = mutableListOf<OperacaoModel>()
-        while(cursor.moveToNext()){
-            val id = cursor.getInt(cursor.getColumnIndexOrThrow("id"))
-            val descricao = cursor.getString(cursor.getColumnIndexOrThrow("descricao"))
-            val valor = cursor.getDouble(cursor.getColumnIndexOrThrow("valor"))
-            val stringTipoOperacao = cursor.getString(cursor.getColumnIndexOrThrow("tipoOperacao"))
-            val tipoOperacao = TipoOperacao.valueOf(stringTipoOperacao);
-            operacaoList.add(OperacaoModel(id,descricao,valor,tipoOperacao))
-        }
-        cursor.close()
-        db.close()
-        return operacaoList
+        val operacaoList = mutableListOf<OperacaoModel>();
+        adicionaLista(cursor, operacaoList);
+
+        cursor.close();
+        db.close();
+        return operacaoList;
     }
 
+    fun adicionaLista(cursor: Cursor, operacaoList: MutableList<OperacaoModel>){
+        while(cursor.moveToNext()){
+            val id = cursor.getInt(cursor.getColumnIndexOrThrow("id"));
+            val descricao = cursor.getString(cursor.getColumnIndexOrThrow("descricao"));
+            val valor = cursor.getDouble(cursor.getColumnIndexOrThrow("valor"));
+            val stringTipoOperacao = cursor.getString(cursor.getColumnIndexOrThrow("tipoOperacao"));
+            val tipoOperacao = TipoOperacao.valueOf(stringTipoOperacao);
+            operacaoList.add(OperacaoModel(id,descricao,valor,tipoOperacao));
+        }
+    }
 }
